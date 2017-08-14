ae792531bd9a48bc1c0385fd56c32f27c4900ed4
milaniadr <ramadhanimilania@gmail.com>
2017-08-14 13:57:12 +0700

    InitialCommit

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { IonicStorageModule } from '@ionic/storage';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Geolocation } from '@ionic-native/geolocation';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { NewPlacePage } from '../pages/new-place/new-place';
+import { PlacesService } from '../services/places.service';
+import { PlacePage } from '../pages/place/place';
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    NewPlacePage,
+    PlacePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDatGJHb8W-Sgw07ymYnc2mW5xRpGFxEV4'
+    })
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    NewPlacePage,
+    PlacePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, PlacesService, Storage, Geolocation
+  ]
+})
+export class AppModule {}
